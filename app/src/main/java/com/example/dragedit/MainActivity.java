3fdaa28d358e9f0c3f0c2f8d042d9ce9b9befc1c
Deiroo <renatoballon.rb@gmail.com>
2021-11-21 02:18:28 -0500

    adding drag & drop

--- app/src/main/java/com/example/dragedit/MainActivity.java
+++ app/src/main/java/com/example/dragedit/MainActivity.java
@@ -2,13 +2,64 @@ package com.example.dragedit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewGroup mainLayout;
+    private ImageView pollito;
+    private ImageView gatito;
+
+    private int xDelta;
+    private int yDelta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainLayout = findViewById(R.id.main);
+        pollito = findViewById(R.id.pollito);
+        gatito = findViewById(R.id.gatito);
+
+        pollito.setOnTouchListener(onTouchListener());
+        gatito.setOnTouchListener(onTouchListener());
+    }
+
+    private View.OnTouchListener onTouchListener() {
+        return new View.OnTouchListener() {
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                final int x = (int) event.getRawX();
+                final int y = (int) event.getRawY();
+
+                switch (event.getAction() & MotionEvent.ACTION_MASK){
+                    case MotionEvent.ACTION_DOWN:
+                        RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams) v.getLayoutParams();
+
+                        xDelta = x - lParams.leftMargin;
+                        yDelta = y - lParams.topMargin;
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) v.getLayoutParams();
+                        layoutParams.leftMargin = x-xDelta;
+                        layoutParams.topMargin = y-yDelta;
+                        layoutParams.rightMargin = 0;
+                        layoutParams.bottomMargin = 0;
+                        v.setLayoutParams(layoutParams);
+                        break;
+                }
+                mainLayout.invalidate();
+                return true;
+            }
+        };
     }
 }
