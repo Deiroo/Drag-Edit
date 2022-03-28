b079f2816f4e5e15120c184f8e2e869fe14afd18
Deiroo <renatoballon.rb@gmail.com>
2022-03-28 13:28:37 -0500

    fixing bugs and adding rotation

--- app/src/main/java/com/example/dragedit/MainActivity.java
+++ app/src/main/java/com/example/dragedit/MainActivity.java
@@ -2,22 +2,14 @@ package com.example.dragedit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.MotionEvent;
-import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     private ViewGroup mainLayout;
-    private ImageView pollito;
-    private ImageView gatito;
-
-    private int xDelta;
-    private int yDelta;
+    private CustomImageView delfin;
+    private CustomImageView gatito;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,41 +17,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mainLayout = findViewById(R.id.main);
-        pollito = findViewById(R.id.pollito);
-        gatito = findViewById(R.id.gatito);
-
-        pollito.setOnTouchListener(onTouchListener());
-        gatito.setOnTouchListener(onTouchListener());
+        delfin = findViewById(R.id.pollito);
     }
 
-    private View.OnTouchListener onTouchListener() {
-        return new View.OnTouchListener() {
-            @SuppressLint("ClickableViewAccessibility")
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-
-                final int x = (int) event.getRawX();
-                final int y = (int) event.getRawY();
-
-                switch (event.getAction() & MotionEvent.ACTION_MASK){
-                    case MotionEvent.ACTION_DOWN:
-                        RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams) v.getLayoutParams();
-
-                        xDelta = x - lParams.leftMargin;
-                        yDelta = y - lParams.topMargin;
-                        break;
-                    case MotionEvent.ACTION_MOVE:
-                        RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) v.getLayoutParams();
-                        layoutParams.leftMargin = x-xDelta;
-                        layoutParams.topMargin = y-yDelta;
-                        layoutParams.rightMargin = 0;
-                        layoutParams.bottomMargin = 0;
-                        v.setLayoutParams(layoutParams);
-                        break;
-                }
-                mainLayout.invalidate();
-                return true;
-            }
-        };
-    }
 }
